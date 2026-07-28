9e663432d009369aaa2ad9f1f76620f52212fb5c
osepulveda1810-design <o.sepulveda1810@gmail.com>
2026-07-28 08:17:06 -0400

    Last Sync: 2026-07-28 08:17 (Mobile)

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,74 @@
-const CACHE_NAME = 'subzero-v2-27';
+const CACHE_NAME = 'subzero-v2-27-pwa';
 const urlsToCache = [
   './',
+  './index.html',
   './manifest.json',
+  './icons/icon-72x72.png',
+  './icons/icon-96x96.png',
+  './icons/icon-128x128.png',
+  './icons/icon-144x144.png',
+  './icons/icon-152x152.png',
   './icons/icon-192x192.png',
-  './icons/icon-512x512.png'
+  './icons/icon-192x192-maskable.png',
+  './icons/icon-256x256.png',
+  './icons/icon-384x384.png',
+  './icons/icon-512x512.png',
+  './icons/icon-512x512-maskable.png',
+  './assets/sprites/player/idle.png',
+  './assets/sprites/player/walk.png',
+  './assets/sprites/player/jump.png',
+  './assets/sprites/player/punch.png',
+  './assets/sprites/player/kick.png',
+  './assets/sprites/player/ice_charge.png',
+  './assets/sprites/player/ice_shoot.png',
+  './assets/sprites/player/kick_air.png',
+  './assets/sprites/player/punch_air.png'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache abierto');
+        return cache.addAll(urlsToCache);
+      })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
   );
 });
